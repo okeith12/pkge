e781ddff9056083ab8c69cd679e5c772bd43f9f8
okeith12 <35121941+okeith12@users.noreply.github.com>
2021-12-04 15:53:08 -0600

    Delete Reverse.go

--- Reverse.go
+++
@@ -1,9 +0,0 @@
-package strutil
-
-func Reverse(s string) string {
-	rev := []rune(s)
-	for i, j := 0, len(rev)-1; i < j; i, j = i+1, j-1 {
-		rev[i], rev[j] = rev[j], rev[i]
-	}
-	return string(rev)
-}
